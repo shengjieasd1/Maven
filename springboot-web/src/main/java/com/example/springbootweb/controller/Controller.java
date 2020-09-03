7343cf26cda345ffb68b22cb1177593e6179fad3
shengjie <408506259@qq.com>
2020-09-03 16:24:35 +0800

    第二次提交

--- springboot-web/src/main/java/com/example/springbootweb/controller/Controller.java
+++ springboot-web/src/main/java/com/example/springbootweb/controller/Controller.java
@@ -1,9 +1,8 @@
 package com.example.springbootweb.controller;
 
 import com.example.springbootutils.helper.CommonMethod;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.junit.Before;
+import org.springframework.web.bind.annotation.*;
 
 import java.text.SimpleDateFormat;
 import java.util.Date;
@@ -11,7 +10,10 @@ import java.util.Date;
 @RestController
 @RequestMapping("/test")
 public class Controller {
-
+    @RequestMapping(value = "/echo/{message}",method = RequestMethod.GET)
+    public String echo(@PathVariable("message") String msg){
+        return "【ECHO】"+msg;
+    }
     @GetMapping("/date")
     public String getDate(){
         Date date = CommonMethod.getCurDate();
