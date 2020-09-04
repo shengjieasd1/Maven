bf6e1d2aa568bda650845333c12fbdc71b2b8f31
shengjie <408506259@qq.com>
2020-09-04 17:25:10 +0800

    数据验证--错误

---
+++ springboot-error/src/main/java/com/example/springbootweb/controller/MemberController.java
@@ -0,0 +1,42 @@
+package com.example.springbootweb.controller;
+
+
+import com.example.springbootweb.util.controller.AbstractBaseController;
+import com.example.springbootweb.vo.Member;
+import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import javax.validation.Valid;
+import java.util.Iterator;
+
+
+@Controller
+public class MemberController extends AbstractBaseController {
+    
+    @RequestMapping(value = "/addPre", method = RequestMethod.GET)
+    public String addPre(){ //增加前的准备操作路径。
+       return "member_add";
+    }
+    
+    @RequestMapping(value = "/add", method = RequestMethod.POST)
+    @ResponseBody
+    public Object add(@Valid Member vo, BindingResult result) {
+        if(result.hasErrors()){
+            Iterator<ObjectError> iterator = result.getAllErrors().iterator();
+            while (iterator.hasNext()){
+                ObjectError error = iterator.next();
+                System.out.println("【错误信息】 code= "+error.getCode()+","+error.getDefaultMessage());
+            }
+            return result.getAllErrors();
+        }else {
+            return vo;
+        }
+    }
+    
+    
+    
+}
